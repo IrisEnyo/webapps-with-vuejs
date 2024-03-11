73d4ef7e03e86171a997e7a78048c28953b35aec
Irina H <ischa15@web.de>
2024-03-11 13:29:47 +0100

    task solved

---
+++ my-first-vue-project/my-first-project/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <MyFirstComponent />
+  <LayoutHeader />
+  <BookList />
+</template>
+
+<script>
+import MyFirstComponent from "./components/MyFirstComponent.vue";
+
+import LayoutHeader from "./components/LayoutHeader.vue";
+
+import BookList from "@/components/BookList.vue";
+
+export default {
+  name: "App",
+  components: {
+    MyFirstComponent,
+    LayoutHeader,
+    BookList,
+  },
+};
+</script>
+
+<style>
+@import url(./assets/global.css);
+</style>
