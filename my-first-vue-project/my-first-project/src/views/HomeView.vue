9386a46869600305087a3d6a99a2869ffc5d12a7
Irina H <ischa15@web.de>
2024-03-20 07:41:32 +0100

    task solved: add router

---
+++ my-first-vue-project/my-first-project/src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+  <LayoutHeader />
+  <BookList />
+</template>
+
+<script>
+import LayoutHeader from "@/components/LayoutHeader.vue";
+
+import BookList from "@/components/BookList.vue";
+
+export default {
+  name: "App",
+  components: {
+    LayoutHeader,
+    BookList,
+  },
+};
+</script>
+
+<style>
+@import url(@/assets/global.css);
+</style>
